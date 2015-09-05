e46752709fc6f55ac8bf2ca1104734d3bef760ed
Takahito Nakano <kano@ponko2.jp>
2015-09-05 16:16:05 +0900

    インデントを調整

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ var eslint = require('gulp-eslint');
 var Elixir = require('laravel-elixir');
 var config = Elixir.config;
 
-Elixir.extend('eslint', function(src, options) {
-    var paths = new Elixir.GulpPaths()
-        .src(src || [
-            config.get('public.js.outputFolder') + '/**/*.js',
-            '!' + config.get('public.js.outputFolder') + '/vendor/**/*.js'
-        ]);
+Elixir.extend('eslint', function (src, options) {
+  var paths = new Elixir.GulpPaths()
+    .src(src || [
+      config.get('public.js.outputFolder') + '/**/*.js',
+      '!' + config.get('public.js.outputFolder') + '/vendor/**/*.js'
+    ]);
 
-    new Elixir.Task('eslint', function() {
-        this.log(paths.src);
+  new Elixir.Task('eslint', function () {
+    this.log(paths.src);
 
-        return gulp.src(paths.src.path)
-            .pipe(eslint(options || {}))
-            .pipe(eslint.format())
-            .pipe(eslint.failAfterError());
-
-    }).watch(paths.src.path);
+    return gulp.src(paths.src.path)
+      .pipe(eslint(options || {}))
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+  }).watch(paths.src.path);
 });
